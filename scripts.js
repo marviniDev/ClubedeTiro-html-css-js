079db99eee1e72525a77cb5ea573870755518049
unknown <vinicius.uchoa2002@gmail.com>
2021-05-17 11:44:38 -0300

    Esse commit atualiza as medidas de px para em

--- scripts.js
+++ scripts.js
@@ -29,16 +29,19 @@ function removeClass(el, className) {
 
 let openMenu = document.querySelector('.button-open-menu');
 let closeMenu = document.querySelector('.button-close-menu');
+let body = document.querySelector('body');
 
 const OpenMenu = () => {
   let menu = document.querySelector('.menu-horizontal');
   menu.classList.remove('menu-close');
+  body.style.overflow = 'hidden';
   menu.classList.add('menu-active');
 };
 
 const CloseMenu = () => {
   let menu = document.querySelector('.menu-horizontal');
   menu.classList.remove('menu-active');
+  body.style.overflow = 'auto';
   menu.classList.add('menu-close');
 };
 
